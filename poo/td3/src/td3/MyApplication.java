889620ff2b21b57267bd6860973d383131eec669
rdelpy <romain.delpy@eut.u-bordeaux.fr>
2020-10-19 15:13:47 +0200

    j'ai jfx chez moi!

---
+++ poo/td3/src/td3/MyApplication.java
@@ -0,0 +1,28 @@
+package td3;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene; 
+import javafx.scene.paint.Color; 
+import javafx.scene.shape.Rectangle; 
+import javafx.stage.Stage; 
+  
+public class MyApplication extends Application { 
+  
+	public void start(Stage stage) throws Exception { 
+		Rectangle rectangle = new Rectangle(150, 100, 200, 100); 
+		rectangle.setFill(Color.BLUE); 
+		
+		Group root = new Group(); 
+		root.getChildren().add(rectangle); 
+		
+		Scene scene = new Scene(root, 400, 300); 
+		
+		stage.setTitle("Klein"); 
+		stage.setScene(scene); 
+		stage.show(); 
+	} 
+  
+	public static void main(String... args) { 
+		Application.launch(args); 
+	} 
+}
